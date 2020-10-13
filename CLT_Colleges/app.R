4c9657cfd2286968dc7404809b10458511277901
gitcnk <58092026+gitcnk@users.noreply.github.com>
2020-10-12 21:47:45 -0400

    Update app.R

--- CLT_Colleges/app.R
+++ CLT_Colleges/app.R
@@ -126,7 +126,7 @@ server <- function(input, output)
       geom_histogram(bins = 40) +
       labs(title = 'Sampling Distribution of the Sample Mean',
            subtitle = '',
-           x = 'sample mean') #+
+           x = 'sample mean') +
       geom_vline(xintercept = pop_mean(), col = 'red', linetype = 'dashed')
   })
   #  }
